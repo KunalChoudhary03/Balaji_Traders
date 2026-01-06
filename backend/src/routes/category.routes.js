b905e31e0271c94dfbbb97853f9559e16780a546
KunalChoudhary03 <kunalchoudhary3344@gmail.com>
2026-01-06 14:10:01 +0530

    Admin removed

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -3,15 +3,9 @@ const router = express.Router();
 const { createCategory, getAllCategories } = require("../controller/category.controller");
 const upload = require("../middleware/upload");
 
-// Multer error handler
-const handleUpload = (req, res, next) => {
-  upload.single("image")(req, res, (err) => {
-    if (err) return res.status(400).json({ success: false, message: err.message });
-    next();
-  });
-};
 
-router.post("/create", handleUpload, createCategory);
+
+router.post("/create", createCategory);
 router.get("/all", getAllCategories);
 
 module.exports = router;
