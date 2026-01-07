f20f46ef1efd38f1a0dcb166080c907e17594180
KunalChoudhary03 <kunalchoudhary3344@gmail.com>
2026-01-07 15:23:05 +0530

    Completed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
+import axios from "axios";
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 connectDB();
 
-// Routes are attached inside src/app.js ("/api/category" and "/api/product")
+const url = `https://balaji-traders-8f7n.onrender.com`;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
