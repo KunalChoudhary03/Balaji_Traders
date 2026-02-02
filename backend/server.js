99a9443031e4c5cd879410a209aad22693e67ad9
KunalChoudhary03 <kunalchoudhary3344@gmail.com>
2026-02-02 13:55:28 +0530

    Some changes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ import connectDB from "./src/db/db.js";
 dotenv.config();
 connectDB();
 
-const url = `https://balaji-traders-8f7n.onrender.com`;
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
